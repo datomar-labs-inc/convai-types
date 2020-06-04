1c95eff4b610527cbbc388c450af01bb87c70fce
Brett Kaiser <Bwkaiser@gmail.com>
2020-06-04 14:05:21 -0500

    added event types

---
+++ event.go
@@ -0,0 +1,35 @@
+package ctypes
+
+// Event is sent from a package to Convai (basically a request)
+type Event struct {
+	ID            string           `json:"id"`
+	ContextTree   ContextTreeSlice `json:"context_tree"`
+	RequestMemory MemoryContainer  `json:"request_memory"`
+}
+
+// Event Request is a request that must be answered with execution result
+type EventRequest struct {
+	Event Event `json:"event"`
+}
+
+// AsyncEventRequest is a request that can be queued and executed without returning the result immediately
+type AsyncEventRequest struct {
+	Events []Event `json:"events"`
+}
+
+//
+type EventResponse struct {
+}
+
+// Async event response is a response to an AsyncEventRequest
+type AsyncEventResponse struct {
+	QueueCount int `json:"queue_count"`
+}
+
+/*type RunnableEvent struct {
+	Name          string           `json:"name"`
+	ID            string           `json:"id"`
+	Documentation string           `json:"documentation"` // Markdown format
+	Style         ctypes.NodeStyle `json:"style"`
+}
+*/
