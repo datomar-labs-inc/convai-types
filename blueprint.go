8ba3b95e9c9c23ac482f96a99d797741835946dc
Caleb Hiebert <calebkhiebert@gmail.com>
2020-09-11 08:34:32 -0500

    update db blueprint

--- blueprint.go
+++ blueprint.go
@@ -7,10 +7,10 @@ import (
 )
 
 type DBBlueprint struct {
-	ID        uuid.UUID    `json:"id"`
-	Modules   DBModuleList `json:"modules"`
-	Version   string       `json:"version"`
-	BotID     uuid.UUID    `json:"bot_id"`
+	ID        uuid.UUID    `db:"id" json:"id"`
+	Modules   DBModuleList `db:"modules" json:"modules"`
+	Version   string       `db:"version" json:"version"`
+	BotID     uuid.UUID    `db:"bot_id" json:"bot_id"`
 	CreatedAt *time.Time   `db:"created_at,omitempty" json:"created_at,omitempty"`
 	UpdatedAt *time.Time   `db:"updated_at,omitempty" json:"updated_at,omitempty"`
 }
@@ -18,6 +18,6 @@ type DBBlueprint struct {
 type DBModuleList []DBModuleListItem
 
 type DBModuleListItem struct {
-	ModuleID uuid.UUID `json:"id"`
-	Version  string    `json:"version"`
+	ModuleID uuid.UUID `db:"id" json:"id"`
+	Version  string    `db:"version" json:"version"`
 }
