9cfdbe253ec54a57eab56e333fd15a4b2c532166
Caleb Hiebert <calebkhiebert@gmail.com>
2020-10-07 13:25:50 -0500

    update blueprint type

--- blueprint.go
+++ blueprint.go
@@ -8,13 +8,35 @@ import (
 	"upper.io/db.v3/postgresql"
 )
 
+type PublishBlueprintRequest struct {
+	BlueprintID uuid.UUID `json:"blueprint_id"`
+	Version     *Semver   `json:"version,omitempty"` // If Version is nil, the latest version will be published
+
+	// If NewVersion is not nil, the old version will be copied into a new unlocked version
+	// If publishing the latest version, a new version must be provided
+	// If not publishing the latest version, the new version must be below the latest version
+	NewVersion *Semver `json:"new_version,omitempty"`
+}
+
 type DBBlueprint struct {
-	ID        uuid.UUID    `db:"id" json:"id"`
-	Modules   DBModuleList `db:"modules" json:"modules"`
-	Version   Semver       `db:"version" json:"version"`
-	BotID     uuid.UUID    `db:"bot_id" json:"bot_id"`
-	CreatedAt *CustomTime  `db:"created_at,omitempty" json:"created_at,omitempty"`
-	UpdatedAt *CustomTime  `db:"updated_at,omitempty" json:"updated_at,omitempty"`
+	ID                   uuid.UUID    `db:"id" json:"id"`
+	Modules              DBModuleList `db:"modules" json:"modules"`
+	Version              Semver       `db:"version" json:"version"`
+	BotID                uuid.UUID    `db:"bot_id" json:"bot_id"`
+	ContinuedFromVersion *Semver      `db:"continued_from_version,omitempty" json:"continued_from_version,omitempty"`
+	Locked               bool         `db:"locked" json:"locked"`
+	CreatedAt            *CustomTime  `db:"created_at,omitempty" json:"created_at,omitempty"`
+	UpdatedAt            *CustomTime  `db:"updated_at,omitempty" json:"updated_at,omitempty"`
+}
+
+type DBBlueprintGraphItem struct {
+	Version   Semver      `db:"version" json:"version"`
+	Parent    *Semver     `db:"parent,omitempty" json:"parent,omitempty"`
+	Locked    bool        `db:"locked" json:"locked"`
+	CreatedAt *CustomTime `db:"created_at,omitempty" json:"created_at,omitempty"`
+	UpdatedAt *CustomTime `db:"updated_at,omitempty" json:"updated_at,omitempty"`
+
+	Children []DBBlueprintGraphItem `db:"-" json:"children"`
 }
 
 func (g DBBlueprint) Value() (driver.Value, error) {
